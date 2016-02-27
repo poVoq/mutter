580fb8ff369096cae627a01e4511ac081a7130c9
todd <todd@maplefish.com>
2016-02-27 17:26:42 -0500

    working

---
+++ config.lua
@@ -0,0 +1,9 @@
+return {
+   channel_name = "Confab Meet",
+   channel_description = "A meeting about this software.",
+   serverpassword = "maroc",
+   max_bandwidth = 720000,
+   max_users = 127,
+   welcome_text = "Hello!",
+   defpermissions = 0xf07ff,
+}
